85a34cc9ba6c3036e521ef518ee390eb47c21e6b
Felix Cederblad <felix_cederblad@hotmail.com>
2025-08-25 14:19:16 +0200

    labels moving with focus

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -7,8 +7,8 @@ interface InputFieldProps {
 export const InputField = ({label}: InputFieldProps): ReactElement => {
   return (
     <section className="input-group">
-      <label htmlFor={label}>{label}</label>
-      <input type="text" placeholder="" className="input-box"></input>
+      <input type="text" placeholder={label} className="input-box"></input>
+      <label htmlFor={label} className="label">{label}</label>
     </section>
   );
 };
